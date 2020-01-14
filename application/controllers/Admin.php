7902d2adb65207998e7970afa8b9614663ae6b08
firmancitranurrahman <firmancitranurrahman@gmail.com>
2020-01-14 08:50:13 +0700

    test

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -45,11 +45,6 @@ Class Admin extends CI_Controller{
         	redirect('Admin/keahlian');
     }
 
-    
-
-
-
-
     public function index(){
         $this->load->view('template_admin/header');
         $this->load->view('template_admin/sidebar');
